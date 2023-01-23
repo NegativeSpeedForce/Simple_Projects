f6acf0205dfbbd34887e5dfe3cd81bf4cfad5bb7
eobard thawne <negative.speed.force0@gmail.com>
2023-01-23 06:02:59 -0800

    modified version of rock paper scissor game.

--- rock paper scissor game.py
+++ rock paper scissor game.py
@@ -5,7 +5,6 @@ def computer_choice():
     choices = ["rock", "paper", "scissor"]
     return random.choice(choices)
 
-print(computer_choice())
 
 def player_choice():
     choices = ["rock", "paper", "scissor"]
@@ -21,39 +20,59 @@ def player_choice():
 player_score = 0
 computer_score = 0
 
+print("Welcome to the game!")
+print("please select one of these commands.")
+
+
+command_help = "command list:\nrounds : enter how many rounds do you wanna play the game.\nexit : exit the programm"
+print("\n\n"+command_help)
+
+
+commands = ["exit", "rounds"]
+
+
+
 
 while True:
-    print("Welcome to the game!")
-    print("To play enter number of round otherwise if you want to exit enter simply exit.")
-    rounds = input("How many round do you want to play?")
-    if rounds == "exit":
+    while True:
+        operation = input("\n\ncommand:")
+        
+        if operation not in commands:
+            print("\n\nplease enter one of command list operation.")
+            print(command_help)
+            
+        elif operation == "rounds":
+            break
+        elif operation == "exit":
+            break
+
+    if operation == "exit":
         break
-    
-    for i in range(int(rounds)):
-        player = player_choice()
-        print(player)
-        computer = computer_choice()
-        print(computer)
+    elif operation == "rounds":
+        rounds = input("please enter number of round do you want to play:")
+        for i in range(int(rounds)):
+            player = player_choice()
+            print(player)
+            computer = computer_choice()
+            print(computer)
+            if computer == "rock" and player == "rock":
+                pass
+            elif computer == "rock" and player == "paper":
+                player_score += 1
+            elif computer == "rock" and player == "scissor":
+                computer_score += 1
+            elif computer == "paper" and player == "paper":
+                pass
+            elif computer == "paper" and player == "rock":
+                computer_score += 1
+            elif computer == "paper" and player == "scissor":
+                player_score += 1
+            elif computer == "scissor" and player == "scissor":
+                pass
+            elif computer == "scissor" and player == "rock":
+                player_score += 1
+            elif computer == "scissor" and player == "paper":
+                computer_score += 1
 
-        if computer == "rock" and player == "rock":
-            pass
-        elif computer == "rock" and player == "paper":
-            player_score += 1
-        elif computer == "rock" and player == "scissor":
-            computer_score += 1
-        elif computer == "paper" and player == "paper":
-            pass
-        elif computer == "paper" and player == "rock":
-            computer_score += 1
-        elif computer == "paper" and player == "scissor":
-            player_score += 1
-        elif computer == "scissor" and player == "scissor":
-            pass
-        elif computer == "scissor" and player == "rock":
-            player_score += 1
-        elif computer == "scissor" and player == "paper":
-            computer_score += 1
-    print("player score:",player_score)
+    print("\n\nplayer score:",player_score)
     print("computer score:", computer_score)
-
-
